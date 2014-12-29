84a46a5008d427161a590c1bbc5f5836e084e062
lkdjiin <xavier.nayrac@gmail.com>
2014-12-29 16:02:00 +0100

    Add cellNextState

--- gol.hs
+++ gol.hs
@@ -21,6 +21,13 @@ replaceChar '1' = '@'
 replaceChar '0' = ' '
 replaceChar c   = c
 
+cellNextState :: Cell -> [Cell] -> Cell
+cellNextState cell neighborhood
+  | total == 4 = cell
+  | total == 3 = 1
+  | otherwise = 0
+    where total = sum neighborhood
+
 main :: IO()
 main =
     let width = 80
