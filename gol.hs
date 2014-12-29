d77e4ae2735fe4cd7f736c337627530a4ffbbda7
lkdjiin <xavier.nayrac@gmail.com>
2014-12-29 15:44:41 +0100

    First commit

---
+++ gol.hs
@@ -0,0 +1,30 @@
+import System.Random
+import Data.List
+
+type Cell = Int
+
+randomCells :: Int -> StdGen -> [Cell]
+randomCells size gen = take size $ randomRs (0, 1) gen
+
+createGeneration :: Int -> [Cell] -> [[Cell]]
+createGeneration _ [] = []
+createGeneration width cells = line:(createGeneration width rest)
+  where (line, rest) = splitAt width cells
+
+formatGeneration :: [[Cell]] -> String
+formatGeneration generation =
+  let rows = intercalate "\n" (map (concatMap show) generation)
+   in map replaceChar rows
+
+replaceChar :: Char -> Char
+replaceChar '1' = '@'
+replaceChar '0' = ' '
+replaceChar c   = c
+
+main :: IO()
+main =
+    let width = 80
+        height = 24
+        cells = randomCells (width * height) (mkStdGen 123)
+        generation = createGeneration width cells
+     in putStrLn $ formatGeneration generation
