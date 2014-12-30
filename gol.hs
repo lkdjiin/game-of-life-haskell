4678293f5071a7b9dc69fd9b0bfed448df067e51
lkdjiin <xavier.nayrac@gmail.com>
2014-12-30 08:31:47 +0100

    Better indent

--- gol.hs
+++ gol.hs
@@ -3,8 +3,8 @@ import GameOfLife
 
 main :: IO()
 main =
-    let width = 80
-        height = 24
-        cells = randomCells (width * height) (mkStdGen 123)
-        generation = createGeneration width cells
-     in putStrLn $ formatGeneration generation
+  let width = 80
+      height = 24
+      cells = randomCells (width * height) (mkStdGen 123)
+      generation = createGeneration width cells
+   in putStrLn $ formatGeneration generation
