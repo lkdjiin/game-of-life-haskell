2b2d4e74b9e7a360ad025647de24c2152f8de272
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-01-01 09:44:54 +0100

    Refactor displayings

--- gol.hs
+++ gol.hs
@@ -3,29 +3,29 @@ import GameOfLife
 import Control.Concurrent
 import qualified Data.Map as M
 import Control.Monad (forM_)
+import Data.Function(on)
+import Data.List(groupBy, intercalate)
 
 displayGrid :: Grid -> IO()
 displayGrid = putStrLn . formatGrid
 
 formatGrid :: Grid -> String
-formatGrid = map replaceChar . fst . M.foldlWithKey reducer ("", -1)
-  where reducer (acc, l) (x, _) v = if x == l
-                                    then (show v ++ acc, l)
-                                    else (show v ++ "\n" ++ acc, x)
+formatGrid = intercalate "\n" . map displayLine . groupByLine . M.toList
+  where groupByLine = groupBy ((==) `on` (fst . fst))
+        displayLine = map (displayCell . snd)
 
-replaceChar :: Char -> Char
-replaceChar '1' = '@'
-replaceChar '0' = ' '
-replaceChar c   = c
+displayCell :: Cell -> Char
+displayCell 1 = '@'
+displayCell 0 = ' '
 
-randomCells :: Int -> StdGen -> [Cell]
-randomCells size = take size . randomRs (0, 1)
+randomCells :: StdGen -> [Cell]
+randomCells = randomRs (0, 1)
 
 main :: IO()
 main =
   let width = 80
       height = 23
-      cells = randomCells (width * height) (mkStdGen 1234)
+      cells = take (width * height) $ randomCells (mkStdGen 1234)
       grid = createGrid width cells
    in forM_ (take 40 $ iterate nextGeneration grid) $ \g -> do
                                                          displayGrid g
